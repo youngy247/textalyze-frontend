f4fb8cd46181b62c826e99f7beca9ce827e06a3c
Adam Young <adamyoungy678@gmail.com>
2024-01-14 22:11:40 +0000

    Feat: Initialize StatisticsDisplay component structure

--- src/components/StatisticsDisplay/StatisticsDisplay.tsx
+++ src/components/StatisticsDisplay/StatisticsDisplay.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 interface Statistics {
   totalWords: number
   averageWordLength: number
@@ -8,3 +10,11 @@ interface Statistics {
 interface StatisticsDisplayProps {
   statistics: Statistics
 }
+
+const StatisticsDisplay: React.FC<StatisticsDisplayProps> = ({ statistics }) => {
+  // const { totalWords, averageWordLength, mostFrequentLength, wordLengthCounts } = statistics;
+
+  return <div>StatisticsDisplay</div>
+}
+
+export default StatisticsDisplay
